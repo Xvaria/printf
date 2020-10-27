4e2d0562f375ddafb3685fcd97a2fb00d8a426ee
Xvaria <2349@holbertonschool.com>
2020-10-27 00:52:39 +0000

    Correction v2.05 fix print recive arguments with backslash

--- _printf.c
+++ _printf.c
@@ -15,47 +15,43 @@ int _printf(const char *format, ...)
         va_list valist;
         int t = 0, a, b;
 
-        if (format && format[0])
-        {
-                va_start(valist, format);
-                for (a = 0; format[a]; a++)
-                {
-                        if (format[a] == '%')
-                        {
-                                if (format[a + 1] == '%')
-                                {
-                                        write(1, "%", 1);
-                                        t++;
-                                        a++;
-                                }
-                                else if (format[a + 1] == 10)
-                                {
-                                        write(1, "%", 1);
-                                        t++;
-                                }
-                                else
-                                {
-                                        for (b = 0; func[b].let; b++)
-                                        {
-                                                if (format[a + 1] == func[b].let)
-                                                {
-                                                        t += func[b].f(valist);
-                                                        a++;
-                                                        break;
-                                                }
-                                        }
-                                        if (!func[b].let)
-                                                return (-1);
-                                }
-                        }
-                        else
-                        {
-                                write(1, &format[a], 1);
-                                t++;
-                        }
-                }
-                va_end(valist);
-                return (t);
-        }
-        return (-1);
+	va_start(valist, format);
+        if (!format || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+	for (a = 0; format[a]; a++)
+	{
+		if (format[a] == '%')
+		{
+			if (format[a + 1] == '%')
+			{
+				write(1, &format[a], 1);
+				t++;
+				a++;
+			}
+			else
+			{
+				for (b = 0; func[b].let; b++)
+				{
+					if (format[a + 1] == func[b].let)
+					{
+						t += func[b].f(valist);
+						a++;
+						break;
+					}
+				}
+				if (!func[b].let)
+				{
+					write(1, &format[a], 1);
+					t++;
+				}
+			}
+		}
+		else
+		{
+			write(1, &format[a], 1);
+			t++;
+		}
+	}
+	va_end(valist);
+	return (t);
 }
